576efb9eeedffc6bdbd77929c7da728d9216913e
Aaron Sky <aaronsky@skyaaron.com>
2015-01-31 15:56:03 -0500

    updated badges to have categories, added UI animations

--- js/app.js
+++ js/app.js
@@ -1,42 +1,53 @@
 (function (angular) {
 	'use strict';
 	
-angular.
-module('BadgeGuideApp', ['ngRoute', 'ngMaterial']).
-factory('Badge', function ($http) {
-		var badges = [];
-		$http.get('js/badges.json')
-			.success(function (data) {
+	var app = angular.module('BadgeGuideApp', ['ngRoute', 'ngMaterial', 'ngAnimate']);
+
+	app.factory('LoadBadges', function ($http) {
+		return $http.get('js/badges.json');
+	});
+
+	app.controller('badgesController', ['$scope', '$mdSidenav', 'LoadBadges', function ($scope, $mdSidenav, LoadBadges) {
+		$scope.searchText = null;
+		$scope.forumsUrl = 'http://forum.starmen.net/forum';
+		$scope.doneloading = false;
+		
+		//Initialize data
+		$scope.badgeList = [];
+		LoadBadges.success(function (data) {
 			for (var i = 0; i < data.length; i++)
 			{
-				badges.push(data[i]);
+				$scope.badgeList.push(data[i]);
 			}
-			}).error(function (data, status, error, config) {
-				badges.push({
-	id:0,
-	name:error,
-	URL:"http://starmen.net/badgevatars/avatars/rofishspy.png",
-	comment:config,
-	title_tag:"",
-	link:"",
-	created_at:"",
-	updated_at:""
-  });
+			$scope.doneloading = true;
+		}).error(function (data, status, error, config) {
+			$scope.badgeList.push({
+				id:0,
+				name:error,
+				URL:"http://starmen.net/badgevatars/avatars/rofishspy.png",
+				comment:config,
+				title_tag:"",
+				link:"",
+				created_at:"",
+				updated_at:""
 			});
-			//badges.push({id:0, name:"dingle", URL:"http://starmen.net/badgevatars/avatars/rofishspy.png", comment:"corner", title_tag:"what"});
-			return badges;
-	}).
-	controller('badgesController', ['$scope', '$mdSidenav', 'Badge', function ($scope, $mdSidenav, Badge) {
-		$scope.badgeFilter = null;
-		$scope.forumsUrl = 'http://forum.starmen.net/forum';
-		$scope.badgeList = Badge;
-		 
+		});
+
+		// Filters
+		$scope.badgeFilter = function (badge) {
+			return (badge.name && badge.URL);
+		};
 		$scope.searchFilter = function (badge) {
-			var keyword = new RegExp($scope.badgeFilter, 'i');
-			return !$scope.badgeFilter || keyword.test(badge.name) || keyword.test(badge.title);
+			var keyword = new RegExp($scope.searchText, 'i');
+			return !$scope.searchText || keyword.test(badge.name) || keyword.test(badge.title) || keyword.test(badge.comment) || keyword.test(badge.category_tags);
 		};
-		$scope.toggleSidenav = function(menuId) {
+		$scope.dateFilter = function (badge) {
+			return parseInt(badge.created_at);
+		};
+
+		//Controls
+		/*$scope.toggleSidenav = function(menuId) {
 			$mdSidenav(menuId).toggle();
-		};
+		};*/
 	}]);
 }(window.angular));
